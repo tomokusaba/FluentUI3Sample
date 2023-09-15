64b7d99c53af061251418e0e729d394408a32faa
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-09-15 19:01:00 +0900

    EditForm例を追加

---
+++ FluentUI3Sample/Data/NumberData.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentUI3Sample.Data
+{
+    public class NumberData
+    {
+        public double Value1 { get; set; }
+        public decimal Value2 { get; set; }
+        public float Value3 { get; set; }
+        public short Value4 { get; set; }
+        public string? Value5  { get; set; }
+    }
+}
