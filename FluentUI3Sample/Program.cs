238ccb044643cfe860e11721653697a919112078
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-12-02 02:37:45 +0900

    FluentUI3.4.1対応

--- FluentUI3Sample/Program.cs
+++ FluentUI3Sample/Program.cs
@@ -10,7 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents(options =>
 {
-    options.HostingModel = BlazorHostingModel.WebAssembly;
+    //options.HostingModel = BlazorHostingModel.WebAssembly;
 });
 
 await builder.Build().RunAsync();
